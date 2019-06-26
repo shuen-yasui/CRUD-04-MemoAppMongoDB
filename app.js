7c9cb7facf196021179f38a76e4f271a33a1d9bb
Shuen Yasui <shuenyasui@gmail.com>
2019-06-25 19:59:32 -0700

    Added modules dotenv mongoose & pug. Replaced index.html with index.pug. Establish connection to MongoDB server. Using boilerplate schema and view.

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
-var express = require("express");
-var app = express();
-var port = 3000;
-var mongoose = require("mongoose");
-
-var memoSchema = new mongoose.Schema({
-  memoBody: String,
+const express = require("express");
+const app = express();
+const path = require('path');
+const port = 3000;
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const memoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+  },
 });
+const Memo = mongoose.model('Registration', memoSchema);
 
-var Memo = mongoose.model("Memo", memoSchema);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE, {useNewUrlParser:true});
+mongoose.Promise = global.Promise;
+mongoose.connection
+  .on('connected', () => {
+    console.log(`Mongoose connection open on ${process.env.DATABASE}`);
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
 
 app.listen(port, () => {
   console.log("Server listening on port " + port);
 });
 
-app.use("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', (req, res) => {
+  Memo.find()
+    .then((notes) => {
+      console.log("notes", notes);
+      res.render('index', { title: 'Listing registrations', notes });
+    })
+    .catch(() => { res.send('Sorry! Something went wrong.'); });
 });
