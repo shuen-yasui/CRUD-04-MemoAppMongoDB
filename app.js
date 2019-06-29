81e80a4503f7c6ee91a68cea68e4cab4f66f16a5
Shuen Yasui <shuenyasui@gmail.com>
2019-06-28 21:27:34 -0700

    Added body-parser module. Added new entry form to view. Added /add route. Changed memoschema to contain author and memo.

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 const port = 3000;
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
 const memoSchema = new mongoose.Schema({
-  name: {
+  author: {
     type: String,
     trim: true,
   },
-  email: {
+  memo: {
     type: String,
     trim: true,
   },
 });
-const Memo = mongoose.model('Registration', memoSchema);
+const Memo = mongoose.model('Memos', memoSchema);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 dotenv.config();
 
 mongoose.connect(process.env.DATABASE, {useNewUrlParser:true});
@@ -36,9 +39,14 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
   Memo.find()
-    .then((notes) => {
-      console.log("notes", notes);
-      res.render('index', { title: 'Listing registrations', notes });
+    .then((memos) => {
+      res.render('index', { title: 'Listing registrations', memos });
     })
     .catch(() => { res.send('Sorry! Something went wrong.'); });
+});
+
+app.post('/add', (req, res) => {
+  const memo = new Memo(req.body);
+  memo.save()
+  res.redirect('/');
 });
